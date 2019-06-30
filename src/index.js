f78e12e37396941682cedb4c7ea4ea339b828d7c
suweya <suweya89@gmail.com>
2019-06-30 19:59:05 +0800

    Tree Shaking

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import _ from "lodash-es";
+import { cube } from "./math";
 
 function component() {
-  let element = document.createElement("div");
+  let element = document.createElement("pre");
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
+    "\n\n"
+  );
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: path.resolve(__dirname, "./src/index.js"),
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
   },
-  plugins: [new htmlWebpackPlugin({ template: "./index.html" })]
+  plugins: [new htmlWebpackPlugin({ template: "./index.html" })],
+  optimization: {
+    usedExports: true
+  }
 };
